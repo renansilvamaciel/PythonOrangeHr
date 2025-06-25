83ae02998e82ff7669ced5bd63a14e7607ee6939
renansilvamaciel <renan.tecnology@gmail.com>
2025-06-25 09:46:33 -0300

    Alterações para demo com o datapoll

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
+from botcity.maestro import BotMaestroSDK
 import os
+
+maestro = BotMaestroSDK()
+
+
+BotMaestroSDK.RAISE_NOT_CONNECTED = False
+
+# Conecta com a BotMaestro
+maestro.login(os.getenv("MAESTRO_SERVER"), os.getenv("MAESTRO_LOGIN"), os.getenv("MAESTRO_KEY"))
 
 resources_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources')
 
