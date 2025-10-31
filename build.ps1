68e9d45022e0b3bc1af9e67547ce9dddcaf5f286
renansilvamaciel <renan.tecnology@gmail.com>
2025-10-31 13:41:16 -0300

    WIP

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 Set-Location -Path (Resolve-Path "$PSScriptRoot\..")
-$exclude = @("venv", ".env", ".gitignore", "temp", "output", "build", "Nav_BeaPro.zip")
+$exclude = @("venv", ".env", ".gitignore", "temp", "output", "build", "PythonOrangeHrm.zip")
 $files = Get-ChildItem -Path . -Exclude $exclude
-Compress-Archive -Path $files -DestinationPath "Nav_BeaPro.zip" -Force
+Compress-Archive -Path $files -DestinationPath "PythonOrangeHrm.zip" -Force
